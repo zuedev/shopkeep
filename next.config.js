7b9ccda20e60f9e0a36dd6d8549db3f77ee396d5
zuedev <zuedev@gmail.com>
2021-01-07 18:50:19 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,12 +2,10 @@ const path = require("path");
 
 module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.resolve = {
-      alias: {
-        components: path.resolve(__dirname, "src/components/"),
-      },
-    };
-
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      components: path.resolve(__dirname, "src/components/"),
+    }
     return config;
   },
 };
